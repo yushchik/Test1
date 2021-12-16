78b182471c771aec4934631683c9d9f13c693155
dy <dy@bam-boo.eu>
2021-12-16 21:58:53 +0300

    added logic for MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
 
@@ -14,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this));
+//        binding.text.setText(R.string.new_text);
+        binding.btn.setOnClickListener(view -> {
+            binding.text.setText(R.string.new_text);
+            binding.text.setTextColor(getColor(R.color.black));
+        });
     }
 }
